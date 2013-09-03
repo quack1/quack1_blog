c7cca3c099fefe18b8fa315399b2297904248b89
Quack1 <alexandre.deloup@gmail.com>
2013-09-03 20:20:02 +0200

    Update theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,7 +5,7 @@ AUTHOR = u'Quack1'
 SITENAME = u'Quack1☠Blog'
 SIDEBAR_DIGEST = u'Blog-Notes of a random Linux/Security/Hack guy'
 SITEURL = 'http://quack1.me'
-RELATIVE_URLS = False
+RELATIVE_URLS = True
 FEED_DOMAIN = SITEURL
 AVATAR = u'static/upload/avatar.png'
 
