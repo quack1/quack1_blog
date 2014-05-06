eadfc6b62f0dc50b399708ed20dca0540ded2d87
quack1 <quack+github@quack1.me>
2014-05-06 08:38:09 +0200

    Make URL absolute

--- publishconf.py
+++ publishconf.py
@@ -12,7 +12,7 @@ DELETE_OUTPUT_DIRECTORY = True
 # Following items are often useful when publishing
 
 # Uncomment following line for absolute URLs in production:
-RELATIVE_URLS = True
+RELATIVE_URLS = False
 
 GOOGLE_ANALYTICS = 'UA-41598568-1'
 DISQUS_SITENAME = 'quack1blog'
